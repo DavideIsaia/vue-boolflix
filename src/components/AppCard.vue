8309975878120d020de7273a16692f1b1b489993
DavideIsaia <davide.enjoy@hotmail.it>
2022-05-24 12:39:24 +0200

    aggiunte bandiere per lingua

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -2,12 +2,22 @@
   <div class="col-6 col-lg-2 mb-4">
     <div class="card">
       <div class="card-body">
-        <h4 class="card-title">{{ movie.title }}</h4>
-        <h4 class="card-title">{{ movie.name }}</h4>
+        <h4 class="card-title">{{ movie.title }} {{ movie.name }}</h4>
         <h5 class="card-subtitle mb-2 text-muted">
-          Titolo originale: {{ movie.original_title }}
+          Titolo Originale: {{ movie.original_title }} {{ movie.original_name }}
         </h5>
-        <h6 class="card-text">Lingua: {{ movie.original_language }}</h6>
+        <h6 class="card-text">
+          Lingua:
+          <span v-if="!langFlags.includes(movie.original_language)">
+            {{ movie.original_language }}
+          </span>
+
+          <img
+            v-else
+            :src="`../assets/flags/${movie.original_language}.png`"
+            alt=""
+          />
+        </h6>
         <h6 class="card-text">Voto: {{ movie.vote_average }}/10</h6>
       </div>
     </div>
@@ -17,6 +27,11 @@
 <script>
 export default {
   name: "AppCard",
+  data() {
+    return {
+      langFlags: ["cn", "de", "en", "es", "fr", "it", "ja", "ru", "pt"],
+    };
+  },
   props: {
     movie: Object,
   },

8c130b02465e229c8c311f53bef3d45a813c3ebd
DavideIsaia <davide.enjoy@hotmail.it>
2022-05-24 15:05:42 +0200

    aggiunta descrizione all'hover sulla locandina

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="col-6 col-md-4 col-lg-3 col-xl-2 mb-4">
+  <div class="col-12 col-md-6 col-lg-4 col-xl-3 mb-4">
     <div class="card">
+      <img
+        class="card-img-top"
+        :src="`https://www.themoviedb.org/t/p/w342${movie.poster_path}`"
+      />
       <div class="card-body">
-        <img
-          class="card-img-top"
-          :src="`https://www.themoviedb.org/t/p/w342${movie.poster_path}`"
-        />
         <h4 class="card-title mt-2">{{ movie.title }} {{ movie.name }}</h4>
         <h5 class="card-subtitle text-muted mb-2">
           Titolo Originale: {{ movie.original_title }} {{ movie.original_name }}
@@ -32,6 +32,9 @@
           >
           </i>
         </h6>
+        <small>
+          {{ movie.overview }}
+        </small>
       </div>
     </div>
   </div>
@@ -58,18 +61,34 @@ export default {
 
 <style lang="scss" scoped>
 .card {
-  border: 2px solid red;
-  margin-bottom: 1rem;
-  background-color: #141414;
+  position: relative;
+  background-color: #060606;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
 
   .flag {
     height: 1.5rem;
   }
 
   .fa-star {
-    color: yellow;
+    color: gold;
     display: inline;
     margin-left: 0.1rem;
+  }
+
+  .card-body {
+    display: none;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  &:hover .card-body {
+    display: block;
+    background-color: rgba($color: #000000, $alpha: 0.8);
   }
 }
 </style>
